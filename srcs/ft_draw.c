5eecd59f20199fb4e5059509d77c79a05d2e20ac
rgero <rgero@student.21-school.ru>
2020-02-16 15:32:16 +0300

    draw 1

--- srcs/ft_draw.c
+++ srcs/ft_draw.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 12:26:07 by rgero             #+#    #+#             */
-/*   Updated: 2020/02/16 15:07:44 by rgero            ###   ########.fr       */
+/*   Updated: 2020/02/16 15:31:17 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,12 @@ void	ft_bresenham(int *pixel, int *pixel1, t_fdf *data)
 	float	w_step;
 	int		max;
 	int		tmp[3];
+	int		color;
 
 	tmp[0] = pixel[0];
 	tmp[1] = pixel[1];
 	tmp[2] = pixel[2];
+	color = (tmp[2] || pixel1[2] ? 0xe80c0c : 0xffffff);
 	h_step = pixel1[0] - tmp[0];
 	w_step = pixel1[1] - tmp[1];
 	max = ft_max(h_step, w_step);
@@ -44,7 +46,7 @@ void	ft_bresenham(int *pixel, int *pixel1, t_fdf *data)
 	w_step = w_step / max;
 	while  ((int)(tmp[0] - pixel1[0]) || (int)(tmp[1] - pixel1[1]))
 	{
-		mlx_pixel_put(data->mlx_ptr, data->win_ptr, tmp[1], tmp[0], 0xffffff);
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, tmp[1], tmp[0], color);
 		tmp[0] += h_step;
 		tmp[1] += w_step;
 	}
@@ -77,17 +79,23 @@ void	ft_draw(t_fdf *data)
 	int	pixel[4][3];
 
 	pixel[0][0] = 0;
-	while (pixel[0][0] < data->height - 1)
+	while (pixel[0][0] < data->height)
 	{
 		pixel[0][1] = 0;
 //		pixel[0][2] = data->in_tab[pixel[0][0]][pixel[0][1]] * data->z_size;
-		while (pixel[0][1] < data->width - 1)
+		while (pixel[0][1] < data->width)
 		{
 			ft_get_pixel(data, pixel[0], pixel[1], -1);
-			ft_get_pixel(data, pixel[0], pixel[2], 0);
-			ft_bresenham(pixel[1], pixel[2], data);
-			ft_get_pixel(data, pixel[0], pixel[3], 1);
-			ft_bresenham(pixel[1], pixel[3], data);
+			if (pixel[0][0] < data->height - 1)
+			{
+				ft_get_pixel(data, pixel[0], pixel[2], 0);
+				ft_bresenham(pixel[1], pixel[2], data);
+			}
+			if (pixel[0][1] < data->width - 1)
+			{
+				ft_get_pixel(data, pixel[0], pixel[3], 1);
+				ft_bresenham(pixel[1], pixel[3], data);
+			}
 			pixel[0][1] = pixel[0][1] + 1;
 		}
 		pixel[0][0] = pixel[0][0] + 1;
